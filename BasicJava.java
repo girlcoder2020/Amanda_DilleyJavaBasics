c127504cef9d5efcaef8594619140faad0dacf81
Amanda <48845910+girlcoder2020@users.noreply.github.com>
2019-06-17 02:22:57 -0500

    uploaded java file
    uploaded JavaBasics file

---
+++ BasicJava.java
@@ -0,0 +1,142 @@
+import java.util.ArrayList;
+
+public class BasicJava {
+
+//	List 1 to 255
+//	public static void main(String[] args) {
+//
+//		for (int i=1; i<256; i++) {
+//		System.out.println(i);
+//		}
+//	}
+	
+//	List odd 1 to 255
+//	public static void main(String[] args) {
+//		for (int i=1; i<256; i++) {
+//			if (i % 2 ==1) {
+//			System.out.println(i);
+//			}
+//		}
+//	}
+	
+//	Sum and index
+//	public static void main(String[] args) {
+//		int sum = 0;
+//		for (int i = 1; i < 256; i++) {
+//			sum += i;
+//			System.out.println("New number:" + i + " Sum:" + sum);
+//		}
+//	}
+	
+//	Sum of numbers
+//	public static void main(String[] args) {
+//		int sum = 0;
+//		int[] arr = {4,10,12,55,60,75};
+//		for (int i = 0; i < arr.length; i++) {
+//			if (i < arr.length) {
+//				sum = sum + arr[i];
+//			}
+//			System.out.println(sum);
+//		}
+//	}
+
+//	Find Max
+//	public static void main(String[] args) {
+//		int[] arr = {4,7,-1,-9,43};
+//		int max = arr[0];
+//		for (int i = 0; i < arr.length; i++) {
+//			if (max < arr[i]) {
+//				max = arr[i];
+//			}
+//		}
+//		System.out.println(max);
+//
+//	}
+	
+//	Average
+//	public static void main(String[] args) {
+//		int sum = 0;
+//		int[] arr = {4,7,1,3,12};
+//		for (int i = 0; i < arr.length; i++) {
+//			if(i < arr.length) {
+//				sum = sum + arr[i];
+//			}
+//		}
+//		System.out.println(sum / arr.length);
+//	}
+	
+//	Odd numbers 1 to 255
+//	public static void main(String[] args) {
+//		ArrayList<Integer> myArray = new ArrayList<Integer>();
+//		for (int i = 0; i < 256; i++) {
+//			if (i % 2 != 0) {
+//				myArray.add(i);
+//			}
+//		}
+//		System.out.print(myArray);
+//	}
+	
+//	Greater than Y
+//	public static void main(String[] args) {
+//		int count = 0;
+//		int[] arr = {3,5,6,8,19,22,31};
+//		int y = 7;
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] > y) {
+//				count++;
+//			}
+//		}
+//		System.out.println(count);
+//	}
+	
+//	Square the values - can't figure out how to print correctly
+//	public static void main(String[] args) {
+//		int[] arr = {2,3,5,6};
+//		for (int i = 0; i < arr.length; i++) {
+//			 arr[i] = (arr[i] * arr[i]);
+//
+//		}
+//		System.out.println(arr);
+//	}
+	
+//	Eliminate negative numbers - can't complete this either
+//	public static void main(String[] args) {
+//		int [] arr = {-2,-6,4,3,8,7};
+//		ArrayList<Integer> myArray = new ArrayList<Integer>();
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] < 0) {
+//				System.out.print(0);
+//			}
+//			else {
+//				System.out.println(i);
+//			}
+//		}
+//	}
+	
+//	min, max, avg
+//	public static void main(String[] args) {
+////		int min = 0;
+////		int max = 0;
+//		int sum = 0;
+//		int[] arr = {2,3,5,7,9};
+//		
+//		for (int i = 0; i < arr.length; i++) {
+////			if (arr[i] < min) {
+////				min = arr[i];
+////			}
+////			else if (arr[i] > max) {
+////				max = arr[i];
+////			}
+//			if (i < arr.length) {
+//			sum = sum + arr[i];
+//			}
+//	System.out.println(sum);
+//		}
+////		System.out.println(min);
+////		System.out.println(max);
+//		System.out.println(sum / arr.length);
+//	}
+	
+	
+	
+}
